fe5f0d495e73f40ac8e17fdacf55a62645119328
Marcie DeFonce <marcie.defonce@gmail.com>
2023-03-13 15:11:00 -0500

    final exercise

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./candidates.component.css']
 })
 export class CandidatesComponent implements OnInit {
-   missionName = "LaunchCode Moonshot"
+   missionName = "LaunchCode Moonshot";
+   editMissionName = false;
 
    candidates = [
     {name: 'Rusty Rutabaga', data: {age: 5, mass: '0.75 kg', sidekick: 'Blake'}, image: 'assets/images/Blake.png'},
@@ -24,9 +25,16 @@ export class CandidatesComponent implements OnInit {
   }
 
   // Code the addToCrew function here:
-
+  addToCrew(person: object) {
+    if(!this.crew.includes(person)) {
+      this.crew.push(person);
+    }
+  }
 
   // BONUS: Code the changeMissionName function here:
-
+changeMissionName(name: string) {
+  this.missionName = name;
+  return false;
+}
 
 }
