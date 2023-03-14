89f0105b6f78ec7e647963daa5a1466debaa2b14
Marcie DeFonce <marcie.defonce@gmail.com>
2023-03-14 18:02:28 -0500

    angular part 3 example

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -6,11 +6,31 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./buttons.component.css']
 })
 export class ButtonsComponent implements OnInit {
-   buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   buttonHeading: string = "Buttons";
+   jokeButtonHeading: string = "Joke Buttons";
+   active: boolean = true;
+   // inactive: boolean = false;
+   // silverInactive: boolean = false;
+   // copperInactive: boolean = false;
+   buttonState: boolean[] = [true, true, true];
+   location: string = "center";
 
    constructor() { }
 
    ngOnInit() { }
+
+   activate() {
+      for(let i = 0; i < this.buttonState.length; i++) {
+         this.buttonState[i] = true;
+      }
+      return this.buttonState;
+   }
+
+   catchMe(oldLocation: string) {
+      while (this.location === oldLocation) {
+         this.location = ['left', 'right', 'center'][Math.floor(Math.random()*3)];
+      }
+     return this.location;
+   }
 
 }
